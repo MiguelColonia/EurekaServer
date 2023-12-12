f5b8409827f0736d9f20cbab15c3f29d75877219
MiguelColonia <96169797+MiguelColonia@users.noreply.github.com>
2023-12-12 22:02:23 +0100

    Add files via upload

---
+++ EurekaServer/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,24 @@
+package com.example.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaServer
+@RestController
+
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+    @RequestMapping(value = "/client")
+    public String home() {
+        return "Eureka Client application";
+    }
+
+
+}
